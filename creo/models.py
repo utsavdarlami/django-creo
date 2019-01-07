981926087198bfa07c7ccd8aed9a952de2dc8a06
utsavdarlami <utsavdarlami17@gmail.com>
2019-01-07 14:43:13 +0545

    Tester model removed

--- creo/models.py
+++ creo/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class Testers(models.Model):
-    user_name = models.CharField(max_length=264,unique=True)
-    first_name  =  models.CharField(max_length=264)
-    last_name = models.CharField(max_length=264)
-    email_id =  models.EmailField(max_length=264,unique=True)
-    def __str__(self):
-        return self.user_name
-
 class UserProfileInfo(models.Model):
     GENDER_CHOICES = (
    ('M', 'Male'),
