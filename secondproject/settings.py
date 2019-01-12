8c9cd58926818181be8d343d7a6a6f05f94d2eb4
Utsav Darlami <utsavdarlami17@gmail.com>
2019-01-12 18:35:31 +0545

    static root removed

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -122,12 +122,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static'),
-     STATIC_ROOT,
      ]
 #media
 MEDIA_URL = '/media/'
