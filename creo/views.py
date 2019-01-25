84787666f1cd7f2cb9313ac180780e6643e400e4
Utsav Darlami <utsavdarlami17@gmail.com>
2019-01-26 01:11:39 +0545

    like button form removed

--- creo/views.py
+++ creo/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from creo.models import UserProfileInfo,PostSubmission,CommentPost,Likes,SavedPost
 from django.contrib import messages
 #importing from  forms.py
-from creo.forms import UserForm,UserProfileInfoForm,CommentPostForm,LikePostButton,PostSubmissionForm,UserProfileInfoUpdateForm
+from creo.forms import UserForm,UserProfileInfoForm,CommentPostForm,PostSubmissionForm,UserProfileInfoUpdateForm
 #importing class views
 from django.views.generic import DeleteView,CreateView,UpdateView
 from django.contrib.auth.models import User
