51d43ec9c82ae570f6513eb053f864a241a18c40
Utsav Darlami <utsavdarlami17@gmail.com>
2019-01-26 01:36:46 +0545

    saved post migrate

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -127,9 +127,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static'),
-     STATIC_ROOT,
-     ]
+    os.path.join(BASE_DIR, 'static'),
+    STATIC_ROOT,
+    ]
 #media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
